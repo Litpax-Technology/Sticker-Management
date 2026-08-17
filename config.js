b628a890ce6a8a9bb8da4b326659d9e45efd088d
Litpax-Technology <mis@litpaxtechnology.com>
2026-08-17 10:22:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 // Paste your deployed Google Apps Script Web App URL here (ends with /exec)
 const CONFIG = {
-  API_URL: "https://script.google.com/a/macros/litpaxtechnology.com/s/AKfycbxmgwoskiXQRAnyxgfsgYM18TE3tGiIz6z84PJJGe2NH1qXXNso2D56itWuo79q9EJP/exec",
+  API_URL: "https://script.google.com/a/macros/litpaxtechnology.com/s/AKfycbz5ERSl4GxrtraTtsdK9mF_i4x4zgD5Tnzvy7lGkhpSWaoKKmCUtQ-2S42yVH6GfK6J/exec",
   APP_NAME: "Sticker Tracker"
 };
